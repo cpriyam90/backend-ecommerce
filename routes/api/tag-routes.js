3b8a323074f3e3c60591d4959ebd8836c240fa03
Priyam Carto <cpriyam90@gmail.com>
2022-05-26 18:05:41 -0400

    sequelize

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../develop/models');
+const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 

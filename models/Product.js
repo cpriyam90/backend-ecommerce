775180d180e05bcfe7dd0c9b77ecbf97cf122ffa
Priyam Carto <cpriyam90@gmail.com>
2022-05-26 17:48:15 -0400

    category

--- models/Product.js
+++ models/Product.js
@@ -1,7 +1,8 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
+const { toDefaultValue } = require("sequelize/types/utils");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -9,14 +10,46 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    productName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate:{
+        isDecimal:true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate:{
+        isNumeric:true
+      },
+      defaultValue:10,
+      
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      references:{
+        model:"category",
+        key:"id"
+      }
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
 

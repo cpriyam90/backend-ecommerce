d34c7ef5a3551a84ecf3bd46ff94e80f405b461b
Priyam Carto <cpriyam90@gmail.com>
2022-05-26 17:34:44 -0400

    initial code

--- config/connection.js
+++ config/connection.js
@@ -4,12 +4,13 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize(process.env.db_name, process.env.db_user, process.env.db_password, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
       },
-    });
+    },
+    console.log("connected"));
 
 module.exports = sequelize;

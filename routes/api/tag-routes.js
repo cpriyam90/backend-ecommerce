a14eedb7ae630b61bf16a4a987aeb32eea988a02
Priyam Carto <cpriyam90@gmail.com>
2022-06-02 20:03:27 -0400

    started with tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,30 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tags = await Tag.findAll ({
+       include: [{
+         model: Product
+       }]
+     })
+     res.status(200).json(tags)
+   } catch (error) {
+     res.status(400).json(error)
+   } 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tag = await Tag.findByPk(req.params.id)
+    res.status(200).json(tag)
+  } catch (error) {
+    res.status(400).json(error)
+  }
 });
 
 router.post('/', (req, res) => {

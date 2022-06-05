f234ec86caae5d343f375c6e872ca71cf589f982
Priyam Carto <cpriyam90@gmail.com>
2022-06-05 17:01:08 -0400

    product routes created

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,31 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const products = await Products.findAll ({
+       include: [{
+         model: Category, Tag
+       }]
+     })
+     res.status(200).json(products)
+   } catch (error) {
+     res.status(400).json(error)
+   } 
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const product = await Product.findByPk(req.params.id)
+    res.status(200).json(product)
+  } catch (error) {
+    res.status(400).json(error)
+  }
 });
 
 // create new product
@@ -89,8 +105,18 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete one product by its `id` value
+  try {
+    const deletedProduct = await Product.destroy({
+      where: {
+        id: req.params.id 
+      }
+    }) 
+    res.json("product has been deleted")
+  } catch (error) {
+    
+  }
 });
 
 module.exports = router;
